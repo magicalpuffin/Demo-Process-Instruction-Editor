65be9b12c657c1042a53271d3bca715024df8205
Michael Li <mealted.lemon@gmail.com>
2023-10-21 18:15:28 -0700

    Created Breadcrumb

--- process_instruction_editor/src/routes/+page.svelte
+++ process_instruction_editor/src/routes/+page.svelte
@@ -1,15 +1,6 @@
 <script lang="ts">
 	import type { JSONContent } from '@tiptap/core';
 	import type { Writable } from 'svelte/store';
-	import TipTap from '$lib/components/TipTap.svelte';
+	import TipTap from '$lib/components/stepDocument/TipTap.svelte';
 	import { writable } from 'svelte/store';
-
-	let testWriteable: Writable<JSONContent> = writable();
-
-	// $: console.log($testWriteable);
 </script>
-
-<h1 class="my-4 text-3xl font-bold">TipTap Testing Area</h1>
-<div class="border">
-	<TipTap editorStore={testWriteable} />
-</div>
--- process_instruction_editor/src/routes/create/+page.svelte
+++ process_instruction_editor/src/routes/create/+page.svelte
@@ -1,24 +1,21 @@
 <script lang="ts">
 	import type { JSONContent } from '@tiptap/core';
 	import type { Writable } from 'svelte/store';
-	import TipTap from '$lib/components/TipTap.svelte';
+
+	import TipTap from '$lib/components/stepDocument/TipTap.svelte';
+	import Breadcrumbs from '$lib/components/Breadcrumbs.svelte';
 	import { writable } from 'svelte/store';
 
 	let testWriteable: Writable<JSONContent> = writable();
 </script>
 
-<div class="breadcrumbs text-sm">
-	<ul>
-		<li><a href="/">Home</a></li>
-		<li><a href="create">Create</a></li>
-	</ul>
-</div>
+<Breadcrumbs />
 
 <h1 class="my-2 text-3xl font-bold">Create Process Instruction</h1>
 <div class="join bg-base-200 flex justify-around rounded-lg shadow-lg">
-	<button class="btn join-item grow">Add Step</button>
-	<button class="btn join-item grow">Add Process</button>
-	<button class="btn join-item grow">Render All</button>
+	<button class="btn join-item grow">Document</button>
+	<button class="btn join-item grow">Data Collection</button>
+	<button class="btn join-item grow">Attachments</button>
 	<button class="btn join-item btn-accent grow">Save</button>
 </div>
 
