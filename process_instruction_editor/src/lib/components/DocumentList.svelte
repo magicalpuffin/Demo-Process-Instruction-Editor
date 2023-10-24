1f2ee4a081c2e0ae9747c223b1ef06bcd76b53ad
Michael Li <mealted.lemon@gmail.com>
2023-10-23 21:16:59 -0700

    added icons, list dropdown

--- process_instruction_editor/src/lib/components/DocumentList.svelte
+++ process_instruction_editor/src/lib/components/DocumentList.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { stepDocument } from '$lib/types/processInstruction';
+	import ThreeDotsIcon from '$lib/icons/ThreeDotsIcon.svelte';
 
 	export let documentList: stepDocument[];
 	export let listTitle: string;
@@ -9,9 +10,19 @@
 	<li class="menu-title">{listTitle}</li>
 	{#each documentList as stepDocument}
 		<li>
-			<a href="/document/{stepDocument.id}">
-				{stepDocument.name}
-			</a>
+			<div class="flex justify-between py-0 pr-0">
+				<a class="grow py-2" href="/document/{stepDocument.id}">
+					{stepDocument.name}
+				</a>
+				<details class="dropdown dropdown-end">
+					<!-- TODO get the button height to fit, fix the 4 pixel gap -->
+					<summary class="btn btn-ghost btn-sm"><ThreeDotsIcon /></summary>
+					<ul class="dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+						<li><a href="/" tabindex="0">Delete</a></li>
+						<li><a href="/" tabindex="0">Something</a></li>
+					</ul>
+				</details>
+			</div>
 		</li>
 	{/each}
 </ul>
